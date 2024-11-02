a9727664b82d47bcf99ecb7b99bb3268bb2a1797
prem12937@gmail.com <prem12937@gmail.com>
2024-11-02 08:47:03 +0530

    feat: add job structure model

---
+++ backend/models/job.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Job struct {
+	gorm.Model
+	JobID              int32  `json:"job_id"`
+	JobRole            string `json:"job_role"`
+	WorkSite           string `json:"work_site"`
+	City               string `json:"city"`
+	Country            string `json:"country"`
+	EmploymentType     string `json:"employment_type"`
+	MinSalary          int32  `json:"min_salary"`
+	MaxSalary          int32  `json:"max_salary"`
+	Profession         string `json:"profession"`
+	Discipline         string `json:"discipline"`
+	ExperienceRequired int8   `json:"experience_required"`
+	JobDescription     string `json:"job_description"`
+}
